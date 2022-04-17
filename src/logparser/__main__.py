95bc1fa13de7a6215ba16dede97781a43a575d64
Minh Nguyen <minhnguyen1205@outlook.com>
2022-04-18 00:56:29 +0700

    Add support for parsing files in specified directory
    - Automatically discover all *.log.gz files in specified directory
    - At the end of program, a summary consists of numbers of files
    discovered, files with found players, found players and errors
    - Revamp some result outputs

--- src/logparser/__main__.py
+++ src/logparser/__main__.py
@@ -1,47 +1,67 @@
 import sys
+import glob
 from . import logparser
 from . import argparser as ap
 
 
 def file_exception_handler(exception, filename):
     if isinstance(exception, FileNotFoundError):
-        print(f'{filename}: File not found')
+        print(f'[ERROR] [{filename}]: File not found')
     elif isinstance(exception, PermissionError):
-        print(f'{filename}: Permission denied')
+        print(f'[ERROR] [{filename}]: Permission denied')
     else:
-        print(f'{filename}: {e}')
+        print(f'[ERROR] [{filename}]: {e}')
 
 
 def print_players_list(players):
-    for p in players:
-        print(f"""    Player: {p['name']}
+    if players:
+        print(f'[{file}]: Found un-whitelisted player(s)')
+        for p in players:
+            print(f"""    Player: {p['name']}
         UUID: {p['id']}
         IP and Port: {p['ip_port']}
         Time: {p['time']}""")
+        print(f'Total: {len(found)} ({file})')
+    else:
+        print(f'[{file}]: No un-whitelisted players found')
 
 
 if __name__ == "__main__":
-    # print(ap.args)
+    print(ap.args)
     if len(sys.argv) == 1:
         ap.parser.print_help()
         sys.exit()
 
-    if len(ap.args['file']) != 0:
-        try:
-            for file in ap.args['file']:
-                found = logparser.parse(file)
-                if found:
-                    print(f'[{file}]: Found un-whitelisted player(s)')
+    if ap.args['dir'] is not None:
+        # Discover all *.log.gz files in the directory
+        discovered = glob.glob(ap.args['dir'] + '/*.log.gz')
+        if len(discovered) != 0:
+            print(f'Discovered {len(discovered)} log file(s) in "{ap.args["dir"]}"')
+            total_players = 0
+            total_error = 0
+            total_found = 0
+            for file in discovered:
+                try:
+                    found = logparser.parse(file)
+                    total_players += len(found)
+                    total_found += 1 if len(found) != 0 else 0
                     print_players_list(found)
-                    print(f'Total: {len(found)} ({file})')
-                else:
-                    print(f'[{file}]: No un-whitelisted players found')
+                except Exception as e:
+                    file_exception_handler(e, file)
+                    total_error += 1
+            print(f'SUMMARY: {len(discovered)} file(s) discovered, {total_found} file(s) found players, \
+{total_players} player(s) found, {total_error} error(s)')
             sys.exit()
-        except Exception as e:
-            file_exception_handler(e, ap.args['file'])
+        else:
+            print(f'No log files found in {ap.args["dir"]}')
             sys.exit(2)
 
-    # if ap.args['dir'] is not None:
-    #     # Discover all *.log.gz files in the directory
-    #     for file in glob.glob(ap.args['dir'] + '/*.log.gz'):
-    #         print(file)
+    if len(ap.args['file']) != 0:
+        for file in ap.args['file']:
+            try:
+                found = logparser.parse(file)
+                print_players_list(found)
+                sys.exit()
+            except Exception as e:
+                file_exception_handler(e, file)
+                sys.exit(2)
--- src/logparser/argparser.py
+++ src/logparser/argparser.py
@@ -2,5 +2,5 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Minecraft Log parser')
 parser.add_argument('file', help='Log file(s) to parse', nargs='*')
-parser.add_argument('-d', '--dir', help='discover and parse all log files in a directory (WIP)')
+parser.add_argument('-d', '--dir', help='discover and parse all log files in a directory')
 args = vars(parser.parse_args())
