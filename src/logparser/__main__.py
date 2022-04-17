f1acf295ce06e6bd5fa3991f7f7bd9f0b90145d5
Minh Nguyen <minhnguyen1205@outlook.com>
2022-04-17 23:52:17 +0700

    Extra revamp the output listing to make reading easier

--- src/logparser/__main__.py
+++ src/logparser/__main__.py
@@ -12,24 +12,36 @@ def file_exception_handler(exception, filename):
         print(f'{filename}: {e}')
 
 
+def print_players_list(players):
+    for p in players:
+        print(f"""    Player: {p['name']}
+        UUID: {p['id']}
+        IP and Port: {p['ip_port']}
+        Time: {p['time']}""")
+
+
 if __name__ == "__main__":
+    # print(ap.args)
     if len(sys.argv) == 1:
         ap.parser.print_help()
         sys.exit()
-    if ap.args['file'] is not None:
+
+    if len(ap.args['file']) != 0:
         try:
             for file in ap.args['file']:
                 found = logparser.parse(file)
                 if found:
-                    print(f'{file}: Found {len(found)} un-whitelisted player(s)')
-                    for player in found:
-                        print(f"""Player: {player['name']}
-    UUID: {player['id']}
-    IP and Port: {player['ip_port']}
-    Time: {player['time']}""")
+                    print(f'[{file}]: Found un-whitelisted player(s)')
+                    print_players_list(found)
+                    print(f'Total: {len(found)} ({file})')
                 else:
-                    print(f'{file}: No un-whitelisted players found')
+                    print(f'[{file}]: No un-whitelisted players found')
             sys.exit()
         except Exception as e:
             file_exception_handler(e, ap.args['file'])
             sys.exit(2)
+
+    # if ap.args['dir'] is not None:
+    #     # Discover all *.log.gz files in the directory
+    #     for file in glob.glob(ap.args['dir'] + '/*.log.gz'):
+    #         print(file)
