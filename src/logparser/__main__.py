101427084a1f57f94c2958d74c6021b81d2664ae
Minh Nguyen <minhnguyen1205@outlook.com>
2022-04-18 22:06:09 +0700

    Code refactor

--- src/logparser/__main__.py
+++ src/logparser/__main__.py
@@ -8,14 +8,14 @@ from . import argparser as ap
 def print_file_exception(exception, filename, ignore_errors):
     if not ignore_errors:
         if isinstance(exception, FileNotFoundError):
-            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + Style.BRIGHT + ': File not found')
+            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + Style.BRIGHT + 'File not found')
         elif isinstance(exception, PermissionError):
             print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + ': ' + Style.BRIGHT + 'Permission denied')
         else:
             print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + ': ' + Style.BRIGHT + str(exception))
 
 
-def print_players_list(players, only_found):
+def print_players_list(players, only_found, file):
     if players:
         print(f'"{file}": ' + Style.BRIGHT + Fore.RED + 'Found un-whitelisted player(s)')
         for p in players:
@@ -29,11 +29,30 @@ def print_players_list(players, only_found):
 
 
 def print_summary(summary):
-    s_total = f'{summary["total"]} file(s) found'
+    s_total = f'{summary["total"]} file(s) searched'
     s_found = ((Style.BRIGHT + Fore.RED) if summary['found'] else Fore.GREEN) + f'{summary["found"]} file(s) found players'
     s_player = ((Style.BRIGHT + Fore.RED) if summary['player'] else Fore.GREEN) + f'{summary["player"]} player(s) found'
     s_error = (Fore.YELLOW if summary['error'] else '') + f'{summary["error"]} error(s)'
     print('SUMMARY: ', s_total, ' | ', s_found, ' | ', s_player, ' | ', s_error)
+
+
+def parse_files(files):
+    summary = dict(
+        total=len(files),
+        found=0,
+        player=0,
+        error=0
+    )
+    for f in files:
+        try:
+            found = logparser.parse(f)
+            summary['player'] += len(found)
+            summary['found'] += 1 if len(found) != 0 else 0
+            print_players_list(found, ap.args['only_found'], f)
+        except Exception as e:
+            print_file_exception(e, f, ap.args['ignore_errors'])
+            summary['error'] += 1
+    return summary
 
 
 if __name__ == "__main__":
@@ -46,34 +65,15 @@ if __name__ == "__main__":
         # Discover all *.log.gz files in the directory
         discovered = sorted(glob.glob(ap.args['dir'] + '/*.log.gz'))
         if len(discovered) != 0:
-            summary = dict(
-                player=0,
-                error=0,
-                found=0,
-                total=len(discovered)
-            )
             print(Style.BRIGHT + f'Discovered {len(discovered)} log file(s) in "{ap.args["dir"]}"')
-            for file in discovered:
-                try:
-                    found = logparser.parse(file)
-                    summary['player'] += len(found)
-                    summary['found'] += 1 if len(found) != 0 else 0
-                    print_players_list(found, ap.args['only_found'])
-                except Exception as e:
-                    print_file_exception(e, file, ap.args['ignore_errors'])
-                    summary['error'] += 1
-            print_summary(summary)
+            smry = parse_files(discovered)
+            print_summary(smry)
             sys.exit()
         else:
             print(f'No log files found in {ap.args["dir"]}')
-            sys.exit(2)
+            sys.exit()
 
     if len(ap.args['file']) != 0:
-        for file in ap.args['file']:
-            try:
-                found = logparser.parse(file)
-                print_players_list(found, ap.args['only_found'])
-                sys.exit()
-            except Exception as e:
-                print_file_exception(e, file, ap.args['ignore_errors'])
-                sys.exit(2)
+        smry = parse_files(ap.args['file'])
+        print_summary(smry)
+        sys.exit()
