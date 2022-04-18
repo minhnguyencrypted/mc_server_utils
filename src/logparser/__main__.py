a0d4f30f1a53afbeaf17d805b344006a387cdc75
Minh Nguyen <minhnguyen1205@outlook.com>
2022-04-18 20:23:19 +0700

    Add CLI colors support for highlighting
    - Using colorama 0.4.4
    - Add requirements.txt and update pyproject.toml

--- src/logparser/__main__.py
+++ src/logparser/__main__.py
@@ -1,34 +1,43 @@
 import sys
 import glob
+from colorama import init, Fore, Style
 from . import logparser
 from . import argparser as ap
 
 
-def file_exception_handler(exception, filename, ignore_errors):
+def print_file_exception(exception, filename, ignore_errors):
     if not ignore_errors:
         if isinstance(exception, FileNotFoundError):
-            print(f'[ERROR] [{filename}]: File not found')
+            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + Style.BRIGHT + ': File not found')
         elif isinstance(exception, PermissionError):
-            print(f'[ERROR] [{filename}]: Permission denied')
+            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + ': ' + Style.BRIGHT + 'Permission denied')
         else:
-            print(f'[ERROR] [{filename}]: {e}')
+            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + ': ' + Style.BRIGHT + str(exception))
 
 
 def print_players_list(players, only_found):
     if players:
-        print(f'[{file}]: Found un-whitelisted player(s)')
+        print(f'"{file}": ' + Style.BRIGHT + Fore.RED + 'Found un-whitelisted player(s)')
         for p in players:
-            print(f"""    Player: {p['name']}
-        UUID: {p['id']}
-        IP and Port: {p['ip_port']}
-        Time: {p['time']}""")
-        print(f'Total: {len(found)} ({file})')
+            print('    Player: ' + Style.BRIGHT + Fore.MAGENTA + p['name'])
+            print('        UUID: ' + p['id'])
+            print('        IP and Port: ' + Fore.YELLOW + p['ip_port'])
+            print('        Time: ' + p['time'])
+        print(Style.BRIGHT + Fore.RED + 'Total: ' + str(len(players)) + Style.RESET_ALL + ' (' + file + ')')
     elif not only_found:
-        print(f'[{file}]: No un-whitelisted players found')
+        print(f'"{file}": ' + Style.BRIGHT + Fore.GREEN + 'No un-whitelisted players found')
+
+
+def print_summary(summary):
+    s_total = f'{summary["total"]} file(s) found'
+    s_found = ((Style.BRIGHT + Fore.RED) if summary['found'] else Fore.GREEN) + f'{summary["found"]} file(s) found players'
+    s_player = ((Style.BRIGHT + Fore.RED) if summary['player'] else Fore.GREEN) + f'{summary["player"]} player(s) found'
+    s_error = (Fore.YELLOW if summary['error'] else '') + f'{summary["error"]} error(s)'
+    print('SUMMARY: ', s_total, ' | ', s_found, ' | ', s_player, ' | ', s_error)
 
 
 if __name__ == "__main__":
-    # print(ap.args)
+    init(autoreset=True)
     if len(sys.argv) == 1:
         ap.parser.print_help()
         sys.exit()
@@ -37,21 +46,23 @@ if __name__ == "__main__":
         # Discover all *.log.gz files in the directory
         discovered = sorted(glob.glob(ap.args['dir'] + '/*.log.gz'))
         if len(discovered) != 0:
-            print(f'Discovered {len(discovered)} log file(s) in "{ap.args["dir"]}"')
-            total_players = 0
-            total_error = 0
-            total_found = 0
+            summary = dict(
+                player=0,
+                error=0,
+                found=0,
+                total=len(discovered)
+            )
+            print(Style.BRIGHT + f'Discovered {len(discovered)} log file(s) in "{ap.args["dir"]}"')
             for file in discovered:
                 try:
                     found = logparser.parse(file)
-                    total_players += len(found)
-                    total_found += 1 if len(found) != 0 else 0
+                    summary['player'] += len(found)
+                    summary['found'] += 1 if len(found) != 0 else 0
                     print_players_list(found, ap.args['only_found'])
                 except Exception as e:
-                    file_exception_handler(e, file, ap.args['ignore_errors'])
-                    total_error += 1
-            print(f'SUMMARY: {len(discovered)} file(s) discovered, {total_found} file(s) found players, \
-{total_players} player(s) found, {total_error} error(s)')
+                    print_file_exception(e, file, ap.args['ignore_errors'])
+                    summary['error'] += 1
+            print_summary(summary)
             sys.exit()
         else:
             print(f'No log files found in {ap.args["dir"]}')
@@ -64,5 +75,5 @@ if __name__ == "__main__":
                 print_players_list(found, ap.args['only_found'])
                 sys.exit()
             except Exception as e:
-                file_exception_handler(e, file, ap.args['ignore_errors'])
+                print_file_exception(e, file, ap.args['ignore_errors'])
                 sys.exit(2)
