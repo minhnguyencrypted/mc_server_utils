e1d5a7c81ce66e97d9753ac222baa0d3c3f6c9ca
Minh Nguyen <minhnguyen1205@outlook.com>
2022-04-19 21:41:22 +0700

    Improve log parsing code

--- src/logparser/logparser.py
+++ src/logparser/logparser.py
@@ -6,21 +6,19 @@ def parse(file_name):
     f = gzip.open(file_name, 'rb')
     for l in f:
         line = l.strip().decode('utf-8')
-        if 'lost connection: You are not whitelisted on this server!' in line:
-            splits1 = line.split('[')
-            # Stripping time from the line
-            time = splits1[1].split(']')[0]
-            # Stripping player name from the line
-            splits2 = splits1[3].split('=')
-            player_id = splits2[1].split(',')[0]
-            player_name = splits2[2].split(',')[0]
-            # Stripping IP address and port number from the line
-            ip_port = splits2[4].split('/')[1].split(')')[0]
-            # found.append(f'{time} {player_id} {player_name} {ip_port}')
+        if line.endswith('lost connection: You are not whitelisted on this server!') and line[12:30] == 'Server thread/INFO':
+            # Extract time
+            time = line[1:9]
+            idname_splits = line.split('=')
+            # Extract UUID and name
+            uuid = idname_splits[1].split(',')[0]
+            name = idname_splits[2].split(',')[0]
+            # Extract IP and port
+            ip_port = idname_splits[4].split('/')[1].split(')')[0]
             found.append({
                 'time': time,
-                'id': player_id,
-                'name': player_name,
+                'id': uuid,
+                'name': name,
                 'ip_port': ip_port
             })
     return found
