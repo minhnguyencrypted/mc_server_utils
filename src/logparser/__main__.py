06f02ba7fbd1088c3f0fa7e5182bda4019428057
Minh Nguyen <minhnguyen1205@outlook.com>
2022-04-22 22:45:35 +0700

    Improve readability of code for printing out results and information
    - Reduce the length of printing functions' code by utilising more
    f-strings

--- src/logparser/__main__.py
+++ src/logparser/__main__.py
@@ -6,26 +6,27 @@ from . import argparser as ap
 
 
 def print_file_exception(exception, filename, ignore_errors):
+    msg = f'{Style.BRIGHT}{Fore.RED}[ERROR] {Style.RESET_ALL}"{filename}":'
     if not ignore_errors:
         if isinstance(exception, FileNotFoundError):
-            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + Style.BRIGHT + 'File not found')
+            print(msg + f'{Style.BRIGHT} File not found')
         elif isinstance(exception, PermissionError):
-            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + ': ' + Style.BRIGHT + 'Permission denied')
+            print(msg + f'{Style.BRIGHT} Permission denied')
         else:
-            print(Style.BRIGHT + Fore.RED + '[ERROR] ' + Style.RESET_ALL + filename + ': ' + Style.BRIGHT + str(exception))
+            print(msg + f'{Style.BRIGHT} {str(exception)}')
 
 
 def print_players_list(players, only_found, file):
     if players:
         print(f'"{file}": ' + Style.BRIGHT + Fore.RED + 'Found un-whitelisted player(s)')
         for p in players:
-            print('    Player: ' + Style.BRIGHT + Fore.MAGENTA + p['name'])
-            print('        UUID: ' + p['id'])
-            print('        IP and Port: ' + Fore.YELLOW + p['ip_port'])
-            print('        Time: ' + p['time'])
-        print(Style.BRIGHT + Fore.RED + 'Total: ' + str(len(players)) + Style.RESET_ALL + ' (' + file + ')')
+            print(f'    Player: {Style.BRIGHT + Fore.MAGENTA}{p["name"]}')
+            print(f'        UUID: {p["id"]}')
+            print(f'        IP and Port: {Fore.YELLOW}{p["ip_port"]}')
+            print(f'        Time: {p["time"]}')
+        print(f'{Style.BRIGHT + Fore.RED}Total: {str(len(players))}{Style.RESET_ALL}')
     elif not only_found:
-        print(f'"{file}": ' + Style.BRIGHT + Fore.GREEN + 'No un-whitelisted players found')
+        print(f'"{file}": {Style.BRIGHT + Fore.GREEN}No un-whitelisted players found{Style.RESET_ALL}')
 
 
 def print_summary(summary):
@@ -33,7 +34,7 @@ def print_summary(summary):
     s_found = ((Style.BRIGHT + Fore.RED) if summary['found'] else Fore.GREEN) + f'{summary["found"]} file(s) found players'
     s_player = ((Style.BRIGHT + Fore.RED) if summary['player'] else Fore.GREEN) + f'{summary["player"]} player(s) found'
     s_error = (Fore.YELLOW if summary['error'] else '') + f'{summary["error"]} error(s)'
-    print('SUMMARY: ', s_total, ' | ', s_found, ' | ', s_player, ' | ', s_error)
+    print('SUMMARY: ', s_total, s_found, s_player, s_error, sep=' | ')
 
 
 def parse_files(files):
